8c921c28f6e7c65822e0fefdf60f90c8416bcea3
Shreyash13-bit <22052762@kiit.ac.in>
2025-05-27 02:05:19 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,15 @@
+@FunctionalInterface
+interface Operation {
+    int operate(int a, int b);
+}
+public class Main {
+    public static int execute(int a, int b, Operation op) {
+        return op.operate(a, b);
+    }
+    public static void main(String[] args) {
+        Operation add = (x, y) -> x + y;
+        Operation multiply = (x, y) -> x * y;
+        System.out.println("Addition: " + execute(10, 5, add));         // Output: 15
+        System.out.println("Multiplication: " + execute(10, 5, multiply)); // Output: 50
+    }
+}
