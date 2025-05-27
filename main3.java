c8064a38d2bf5e3ceb82cf5bbad70cd40145bed1
Shreyash13-bit <22052762@kiit.ac.in>
2025-05-27 05:54:56 +0530

    Update main3.java

--- main3.java
+++ main3.java
@@ -1,20 +1,13 @@
 import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
-
 public class main3 {
     public static void main(String[] args) {
         List<String> names = Arrays.asList("John", "Alice", "Jack", "Bob", "Jill", "Steve");
-
-        // Define the predicate
         Predicate<String> startsWithJ = name -> name.startsWith("J");
-
-        // Use stream to filter names
         List<String> filteredNames = names.stream()
                                           .filter(startsWithJ)
                                           .collect(Collectors.toList());
-
-        // Output the result
         System.out.println("Names starting with J: " + filteredNames);
     }
 }
