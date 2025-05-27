55d784fd7f0bc88705f83dbccbafccab94e0e499
Shreyash13-bit <22052762@kiit.ac.in>
2025-05-27 05:55:22 +0530

    Update main4.java

--- main4.java
+++ main4.java
@@ -1,17 +1,10 @@
 import java.util.*;
 import java.util.function.Predicate;
-
 public class main4 {
     public static void main(String[] args) {
         List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
-
-        // Define a predicate to check for even numbers
         Predicate<Integer> isEven = n -> n % 2 == 0;
-
-        // Remove even numbers using removeIf
         numbers.removeIf(isEven);
-
-        // Output the list
         System.out.println("List after removing even numbers: " + numbers);
     }
 }
